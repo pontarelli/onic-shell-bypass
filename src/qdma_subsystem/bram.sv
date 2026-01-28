aeb5fa764bdf9e486630d1ac74080cf2f9161a5f
SP&AM <sp@node119>
2026-01-28 08:45:51 +0000

    modified bram with internal clear due to synth issues

--- src/qdma_subsystem/bram.sv
+++ src/qdma_subsystem/bram.sv
@@ -2,26 +2,56 @@ module qid_packet_counter #(
     parameter ADDR_WIDTH = 11,
     parameter DATA_WIDTH = 32
   ) 
-(clk, we,addr,dout);
-    input clk;
-    input we;
-    input [ADDR_WIDTH-1:0] addr;
-    output reg [DATA_WIDTH-1:0] dout;
+(rst,clka,clkb, wea,web,addra,addrb,din,douta,doutb);
+    
+    input [ADDR_WIDTH-1:0] addra;
+    input clka;
+    input wea;
+    input [DATA_WIDTH-1:0] din;
+    output reg [DATA_WIDTH-1:0] douta;
+    
+    input clkb;
+    input rst;
+    input web;
+    input [ADDR_WIDTH-1:0] addrb;
+    output reg [DATA_WIDTH-1:0] doutb;
+    wire [ADDR_WIDTH-1:0] addr;
     reg [DATA_WIDTH-1:0] ram [0:(1<<ADDR_WIDTH)-1];
-
-    always @(posedge clk) begin
-        if (we)
-            ram[addr] <= ram[addr] + 1;
-
-        dout <= ram[addr];
+    reg clear;
+    reg [ADDR_WIDTH-1:0] internal_counter;
+    
+    
+    always @(posedge clka) begin
+        douta = ram[addra];    
+    end
+    
+    always @(posedge clka) begin
+        if (wea)
+            clear = din[0];    
+    end
+    
+    always @(posedge clkb) begin
+        if (rst)
+            internal_counter = 0;
+        else
+            internal_counter = internal_counter +1;                 
+    end
+    
+    assign addr= (clear) ? internal_counter : addrb; 
+    
+    always @(posedge clkb) begin
+        if (web | clear)
+            ram[addr] = (clear)? 0 : ram[addr] + 1;
+        doutb = ram[addrb];
     end
 
-endmodule;
+endmodule
 
 module qid_ram
-(clk, we, addra, addrb, din, douta, doutb);
+(clka, clkb, we, addra, addrb, din, douta, doutb);
 
-    input clk;
+    input clka;
+    input clkb;
     input we;
     input [10:0] addrb;
     output reg [127:0] doutb;
@@ -34,8 +64,11 @@ module qid_ram
     reg [31:0] ram1 [0:(1<<11)-1];
     reg [31:0] ram0 [0:(1<<11)-1];
 
-    always @(posedge clk)  begin
+    
+    always @(posedge clkb)
         doutb = {ram3[addrb], ram2[addrb], ram1[addrb], ram0[addrb]};
+        
+    always @(posedge clka)  begin
         if (we) begin
             if (addra[3:2] == 2'b00)
                 ram0[addra[14:4]] <= din;
